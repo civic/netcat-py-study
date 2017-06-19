b98ab0f74337272ba0ce5bcaedef07b4a8c6912e
civic <tsskmail@gmail.com>
2017-06-19 13:53:04 +0900

    正常に終了できるように修正

--- nc-client-thread.py
+++ nc-client-thread.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import socket
 import threading
+import queue
 from contextlib import contextmanager
 
 def parse_args():
@@ -20,27 +21,26 @@ def connect_to_host(host, port):
 
 def receiver(sock):
     while True:
-        from_server = sock.recv(8)
+        from_server = sock.recv(1024)
         sys.stdout.buffer.write(from_server)
         sys.stdout.flush()
 
-def sender(sock):
-    for line in sys.stdin.buffer:
-        sock.send(line)
 
 def main():
     args = parse_args()
 
-    # TODO: きれいに停止できてない
     with connect_to_host(args.host, args.port) as sock:
+
         th_r = threading.Thread(target=receiver, args=[sock])
+        th_r.setDaemon(True)
         th_r.start()
 
-        th_s = threading.Thread(target=sender, args=[sock])
-        th_s.start()
+        try:
+            for line in sys.stdin.buffer:
+                sock.send(line)
+        except KeyboardInterrupt:
+            sock.close()
 
-        th_r.join()
-        th_s.join()
 
 
 if __name__ == "__main__":
