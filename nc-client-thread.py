17f3de27e932f3deedfbc1ee67001188b72317fc
civic <tsskmail@gmail.com>
2017-06-20 09:47:56 +0900

    コード整理

--- nc-client-thread.py
+++ nc-client-thread.py
@@ -2,8 +2,22 @@ import argparse
 import sys
 import socket
 import threading
-import queue
 from contextlib import contextmanager
+
+def main():
+    args = parse_args()
+
+    with connect_to_host(args.host, args.port) as sock:
+
+        th_r = threading.Thread(target=receiver, args=(sock,))
+        th_r.setDaemon(True)
+        th_r.start()
+
+        try:
+            for line in sys.stdin.buffer:
+                sock.send(line)
+        except KeyboardInterrupt:
+            sock.close()
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -24,23 +38,6 @@ def receiver(sock):
         from_server = sock.recv(1024)
         sys.stdout.buffer.write(from_server)
         sys.stdout.flush()
-
-
-def main():
-    args = parse_args()
-
-    with connect_to_host(args.host, args.port) as sock:
-
-        th_r = threading.Thread(target=receiver, args=[sock])
-        th_r.setDaemon(True)
-        th_r.start()
-
-        try:
-            for line in sys.stdin.buffer:
-                sock.send(line)
-        except KeyboardInterrupt:
-            sock.close()
-
 
 
 if __name__ == "__main__":
