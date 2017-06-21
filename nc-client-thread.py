c669329ce7072cf7c2c5d5c9d86b61074cbb0451
civic <tsskmail@gmail.com>
2017-06-21 15:08:29 +0900

    サーバ側からの切断を検出するように修正

--- nc-client-thread.py
+++ nc-client-thread.py
@@ -3,21 +3,35 @@ import sys
 import socket
 import threading
 from contextlib import contextmanager
-
+import select
+import queue
 def main():
     args = parse_args()
 
     with connect_to_host(args.host, args.port) as sock:
 
-        th_r = threading.Thread(target=receiver, args=(sock,))
-        th_r.setDaemon(True)
-        th_r.start()
+        disconnected = threading.Event()
 
         try:
-            for line in sys.stdin.buffer:
-                sock.send(line)
+            th_r = threading.Thread(target=receiver, args=(sock,disconnected))
+            th_r.setDaemon(True)
+            th_r.start()
+
+            # server側から先に切断されても標準入力readがblockされたままでexitしない
+            # 標準入力になにか書き込めば停止する
+            for buff in sys.stdin.buffer:
+                if not buff:
+                    disconnected.set()
+                    break
+                if disconnected.is_set():
+                    break
+
+                sock.send(buff)
+
+            print("finish")
+
         except KeyboardInterrupt:
-            sock.close()
+            pass
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -32,12 +46,22 @@ def connect_to_host(host, port):
         s.connect((host, port))
         yield s
 
-
-def receiver(sock):
+def receiver(sock,disconnected):
     while True:
+
+        if disconnected.is_set():
+            break
+
         from_server = sock.recv(1024)
+        if not from_server:
+            break
+
         sys.stdout.buffer.write(from_server)
         sys.stdout.flush()
+
+    print("disconnect")
+    disconnected.set()
+    sys.exit()
 
 
 if __name__ == "__main__":
